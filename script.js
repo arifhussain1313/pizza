4f26c46dfd1a2f951694bd3a8cdeafb6e3b0b89f
arifhussain1313 <zakiriinguti@gmail.com>
2024-06-20 22:02:58 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// var row = 5;
+// var cols = 5;
+// for (let i = 5; i >= 1; i--) {
+//     for(let j = i; j >= 1; j--){
+//         document.write("*");
+//         console.log("pizza");
+//         alert("pizza");
+//     }
+//     document.write("<br/>");
+// }
+
+var email = "example@example.com";
+alert("My email addres is " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+document.getElementById("demo").innerHTML = "Yah! I can write HTML content through JavaScript";
